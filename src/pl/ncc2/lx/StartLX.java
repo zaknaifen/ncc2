4f5ff47d6df5e035fe9772490bcf015de982ad66
zaknaifen <zaknaifen@gmail.com>
2016-09-29 17:22:17 +0200

    Lx_cmd_v_0_1

---
+++ src/pl/ncc2/lx/StartLX.java
@@ -0,0 +1,47 @@
+package pl.ncc2.lx;
+
+//importy
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.InputStreamReader;
+
+
+
+public class StartLX {
+
+	static File dir =null;
+	static BufferedReader is=null;
+	static BufferedReader es=null;
+	static String LxCommand ="/srv_app/./";
+	
+	public StartLX() {
+		
+	}
+	private static void ProcessLx(String cmd){
+		
+		try{
+			Process process;
+			if(dir != null){return;}
+			else{
+				process = Runtime.getRuntime().exec(LxCommand+cmd);
+				String line;
+				is = new BufferedReader(new InputStreamReader(process.getInputStream()));
+				
+				
+			}
+				
+			
+			
+		}catch(Exception e)
+		{
+			
+		}
+		
+		
+		
+	}
+	
+	
+	
+}
